e7dabff77000a91f7d550fc9a722dbac49d21375
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2023-05-29 21:45:41 +0430

    Exercise - stopwatch.

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-//private properties and method.
-function Circle(radius){
-    this.radius = radius;
+// Exercise - stopwatch
 
-    let defaultLocation = {x: 0, y: 0};
+//constructor function
+function Stopwatch() {
+  //private variables
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-    this.getDefaultLocation = function(){
-        return defaultLocation;
-    }
+  this.start = function () {
+    if (running) throw new Error("stopwatch has already started...");
+    running = true;
 
-    //abstraction - getter and setter
-    Object.defineProperty(this, 'defaultLocation', {
-    get: function(){
-        return defaultLocation;
+    startTime = new Date();
+  };
+  this.stop = function () {
+    if (!running) throw new Error("stopwatch is not started...");
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration = +seconds;
+  };
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+  this.reset = function () {};
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
     },
-    set: function(value){
-        if(!value.x || !value.y)
-           throw new Error('invalid location...');
-        defaultLocation = value;
-    }
-})
-
-    this.draw = function(){
-        console.log('draw');
-    };
+  });
 }
 
-const circle = new Circle(10);
-circle.draw();
+// //*************simple implementation */
+// function Stopwatch() {
+//   let startTime,
+//     endTime,
+//     duration = 0;
 
-circle.defaultLocation = 1;
-circle.getDefaultLocation();
+//   this.start = function () {
+//     startTime = new Date();
+//   };
+//   this.stop = function () {
+//     endTime = new Date();
 
+//     const seconds = endTime.getSeconds() - startTime.getSeconds();
+//     duration += seconds;
+//   };
 
+//   this.reset = function () {
+//     startTime = null;
+//     endTime = null;
+//     duration = 0;
+//   };
 
-
-
-
-
-
-
-
-
-
-
-
-
+//   Object.defineProperty(this, "duration", {
+//     get: function () {
+//       return duration + " seconds";
+//     },
+//   });
+// }
