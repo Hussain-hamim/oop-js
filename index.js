f4793871938953dd2bd72eea5768629c2ef39799
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2023-05-29 16:46:58 +0430

    abstraction...

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-//enumrating though object
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log("draw");
-  };
+//hide the details only show the essential.
+function Circle(radius){
+    this.radius = radius;
+this.defaultLocation = {x: 0, y: 0};
+this.computeOptimumLocation = function(){
+    console.log('compute method');
+    //...
+}
+
+    this.draw = function(){
+        this.computeOptimumLocation();
+
+        console.log('draw');
+    };
 }
 
 const circle = new Circle(10);
 
-//to get key in the object
-for (let key in circle) {
-  console.log(key);
-}
+circle.draw();
 
-//to get key value in the object we use the bracket notation
-for (let key in circle) {
-  console.log(key, circle[key]);
-}
 
-//another to access key in object
-const keys = Object.keys(circle);
-console.log(keys);
 
-//check for existing a property in an object we use (if - in)
-if('radius' in circle){
-    console.log('circle has a radius');
-}
+
+
+
+
+
+
+
+
+
+
+
 
