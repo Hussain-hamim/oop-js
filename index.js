ea3cccbbc9f0c9ff2cc01ac0e82a0ede324053c6
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2023-05-31 15:28:55 +0430

    stopwatch with interface.

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-// prototype vs instance member
+///-----------------counter or stopwatch----------------------------
 
-function Circle(radius){
-   //instance member
-   this.radius = radius;
-   this.move = function(){
-     // this.draw();
-      console.log('move...');
-   }
+
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+
+  this.start = function () {
+    if (running) throw new Error("Stopwatch has already started.");
+
+    running = true;
+
+    startTime = new Date();
+      // document.getElementById("p").innerHTML = "counter: " + startTime.getSeconds();
+
+  };
+
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch is not started.");
+
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+    document.getElementById("p").innerText = "counter: ";
+  };
+
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
+};
+
+function dur(){
+
+   document.getElementById('p').innerText ="counter: "+ sw.duration;
 }
 
-//prototype member
-Circle.prototype.draw = function(){
-   this.move();
-   console.log('draw...');
-}
-
-const c1 = new Circle(1);
-const c2 = new Circle(1);
-
-console.log(c1.draw());
-
-Circle.prototype.toString = function(){
-   return 'circle with radius ' + this.radius;
-}
-
-console.log(c2.toString());
+const sw = new Stopwatch();
